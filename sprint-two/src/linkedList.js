3abee92b7851416af6841894198a34ffcd502ffb
Kevin Le <kevinle@Kevins-MacBook-Pro-2.local>
2019-04-12 16:26:53 -0700

    linkList and tree completed

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,41 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  
 
   list.addToTail = function(value) {
+    var newNode = Node(value); // create new node that stores the value 
+    
+    if(list.head === null && list.tail === null) { // if it is a new list 
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+        list.tail.next = newNode;
+        list.tail = newNode;
+    }
+  
   };
 
   list.removeHead = function() {
+    removeNode = list.head.value
+    list.head = list.head.next
+    return removeNode
   };
 
   list.contains = function(target) {
+    var contains = false; 
+    var temp = this.head;
+    while(contains === false) {
+      if(temp.value === target) {
+        contains = true;
+      } else {
+        if(temp.next === null) {
+          break;
+        }
+        temp = temp.next;
+      }
+    }
+    return contains
   };
 
   return list;
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree, treeMethods)
+  newTree.children = [];  // fix me
+
 
   return newTree;
 };
@@ -11,9 +13,22 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+	// create a new tree w/ value 
+	var child = Tree(value);
+	//push new tree into children array 
+	this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+	if(this.value === target) {
+		return true;
+	}
+	for(var i =0; i < this.children.length; i++) {
+		if(this.children[i].contains(target)) {
+			return true
+		}
+	}
+	return false;
 };
 
 
